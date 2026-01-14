4bf9aa16e18ca329aca8d7ee33c64809b7a026f3
Your Name <you@example.com>
2026-01-14 11:34:14 +0000

    add all

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,9 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://sigobc.altdigit.africa/api',
+});
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
